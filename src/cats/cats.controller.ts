bb6188fb27116e4d957f32e5d4c4ec346d9bc6df
Ralph Effting <ralfting@gmail.com>
2019-11-10 20:29:00 -0300

    Create a forbbiden exception

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -4,6 +4,8 @@ import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
 
+import { ForbiddenException } from '../common/exceptions/forbidden.exception';
+
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
@@ -15,6 +17,7 @@ export class CatsController {
 
   @Get()
   async findAll(): Promise<Cat[]> {
+    // throw new ForbiddenException();
     return this.catsService.findAll();
   }
 }
