48e988973a15ea31b4f1042dd5cfaaab7146769b
Ralph Effting <ralfting@gmail.com>
2019-11-10 15:01:23 -0300

    Create Cat module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsService} from './cats/cats.service';
 
-import { CatsController } from './cats/cats.controller';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  imports: [CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
